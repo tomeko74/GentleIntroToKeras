515e85e259407822e31bfd6e581754d4bd4032ba
tomeko74 <thomasz.orlowski@gmail.com>
2018-12-05 00:02:58 +0100

    End

--- gentleintrotokeras.py
+++ gentleintrotokeras.py
@@ -17,13 +17,14 @@ Y = array[:,11]
 dataframe.head()
 
 model = Sequential()
-model.add(Dense(12, input_dim=11, kernel_initializer='uniform', activation='relu'))
+model.add(Dense(16, input_dim=11, kernel_initializer='uniform', activation='relu'))
+model.add(Dense(8, kernel_initializer='uniform', activation='relu'))
 model.add(Dense(8, kernel_initializer='uniform', activation='relu'))
 model.add(Dense(1, kernel_initializer='uniform', activation='sigmoid'))
 
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-model.fit(X, Y, epochs=200, batch_size=30)
+model.fit(X, Y, epochs=300, batch_size=50)
 
 scores = model.evaluate(X, Y)
 print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
